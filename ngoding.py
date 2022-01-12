b29483c629b60ad50a487266c3ca8032a4c0e654
wulandarikaranamu <wulandarikaranamu02@gmail.com>
2022-01-12 19:38:53 +0700

    halo

---
+++ ngoding.py
@@ -0,0 +1,77 @@
+#merry wulandari
+#fungsi dengan operator aritmatika
+
+def huruf (a,b):
+    def abjad (c,d):
+        return c + d
+    return abjad (a,b)
+
+nilai = huruf(4,2)
+print(nilai)
+
+def huruf (a,b):
+    def abjad (c,d):
+        return c - d
+    return abjad (a,b)
+
+nilai = huruf(8,4)
+print(nilai)
+
+def huruf (a,b):
+    def abjad (c,d):
+        return c * d
+    return abjad (a,b)
+
+nilai = huruf(2,2)
+print(nilai)
+
+def huruf (a,b):
+    def abjad (c,d):
+        return c / d
+    return abjad (a,b)
+
+nilai = huruf(12,6)
+print(nilai)
+
+def huruf (a,b):
+    def abjad (c,d):
+        return c ** d
+    return abjad (a,b)
+
+nilai = huruf(25,5)
+print(nilai)
+
+def huruf (a,b):
+    def abjad (c,d):
+        return c // d
+    return abjad (a,b)
+
+nilai = huruf(15,3)
+print(nilai)
+
+def huruf (a,b):
+    def abjad (c,d):
+        return c % d
+    return abjad (a,b)
+
+nilai = huruf(10,5)
+print(nilai)
+              
+        
+
+              
+        
+ 
+        
+
+              
+        
+
+              
+        
+
+              
+        
+
+              
+        
